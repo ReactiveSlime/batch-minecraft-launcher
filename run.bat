52be48b54543e317f748b54cfb45fcef70337908
ReactiveSlime <33995597+ReactiveSlime@users.noreply.github.com>
2019-08-19 18:23:46 +1000

    added update change log if new update is downloaded

--- run.bat
+++ run.bat
@@ -4,6 +4,22 @@ rem gets latest version
 echo Getting Latest Version
 curl -o run.bat "https://raw.githubusercontent.com/ReactiveSlime/batch-minecraft-launcher/master/run.bat" -L
 
+rem check if latest version is downloaded
+cls
+
+if not exist version.txt (
+echo|set /p="0">Version.txt
+)
+set current_build=1
+set /p build=<version.txt
+if %build% EQU %current_build% (
+goto main
+) else (
+goto update
+echo|set /p="1">Version.txt
+pause
+)
+
 rem main menu
 :main
 cls
@@ -22,6 +38,7 @@ if %login% EQU admin goto admincheck
 goto main
 
 rem log in
+rem checks if account exist
 :login
 cls
 set /p name=Username: 
@@ -29,50 +46,26 @@ if exist %name%.bat (
 call %name%.bat
 ) else (
 echo Invalid Username
+pause>nul
 goto main
 )
-
-if not %username% EQU %name% (
+rem makes username case sensitive
+if not %name% EQU %username% (
 echo Invalid Username
 pause>nul
 goto main
 )
+rem password check. also case sensitive
 set /p pass=Password: 
-if not %password% EQU %pass% (
+if not %pass% EQU %password% (
 echo That Is Not A valid Password.
 pause>nul
 goto main
 )
 goto loginsettings
 
-rem creates user
-:createuser
-cls
-echo.
-echo What Would You Like Your Username To Be?
-set /p username= 
-
-if exist "%username%.bat" (
-echo The Account %username% Already Exist.
-pause>nul
-goto main
-)
-
-echo.
-echo What Would You Like Your Password To Be?
-set /p password= 
-(
-echo set username=%username%
-echo set password=%password%
-)>"%username%.bat"
-mkdir "%cd%\data\%username%"
-attrib +H +S "%username%.bat"
-goto main
-
-
 rem user settings
 :loginsettings
-
 cls
 echo What Would You Like To Do?
 echo.
@@ -102,6 +95,7 @@ goto loginsettings
 
 rem change users password
 :changepassword
+rem asks user for current password  
 set /p pass=Please Enter Your Current Password:
 call %name%.bat
 if not %password% EQU %pass% (
@@ -109,6 +103,7 @@ echo Invalid Password.
 pause>nul
 goto loginsettings
 )
+rem ask user for new password
 echo What Would Uou Like To Change Your Password To?
 set /p password=
 echo.
@@ -131,6 +126,33 @@ echo Account Deleted
 pause>nul
 goto main
 
+rem creates user
+:createuser
+cls
+echo.
+echo What Would You Like Your Username To Be?
+echo Username can't contain spaces
+set /p username= 
+rem checks if account already exist
+if exist "%username%.bat" (
+echo The Account %username% Already Exist.
+pause>nul
+goto main
+)
+
+echo.
+echo What Would You Like Your Password To Be?
+echo Password can't contain spaces
+set /p password= 
+rem creats file with user info
+(
+echo set username=%username%
+echo set password=%password%
+)>"%username%.bat"
+mkdir "%cd%\data\%username%"
+attrib +H +S "%username%.bat"
+goto main
+
 rem admin password check
 :admincheck
 call admin.bat
@@ -149,28 +171,19 @@ rem admin main menu
 cls
 echo What Would You Like To Do?
 echo.
-echo 1) List Users
+echo 1) Main Menu
 echo 2) Delete User
-echo 3) Main Page
+echo 3) List Users
 echo 4) Change User Password (WIP)
 echo 5) Change Admin Password
 echo.
 echo.
 set /p admin=
-if %admin% EQU 1 goto list
+if %admin% EQU 1 goto main
 if %admin% EQU 2 goto delete
-if %admin% EQU 3 goto main
+if %admin% EQU 3 goto list
 if %admin% EQU 4 goto changeuserpassword
 if %admin% EQU 5 goto adminpassword
-goto admin
-
-rem admin lists all users
-:list
-cls
-echo All Users
-for /F "delims= eol=" %%A IN ('dir /AS /B') do echo %%~nA
-echo Press Any Key To Continue
-pause >nul
 goto admin
 
 rem admin deletes user
@@ -186,18 +199,26 @@ goto admin
 del /AS "%name%.bat"
 del /F/Q/S "%cd%\data\%username%\*.*" > nul
 rmdir /Q/S "%cd%\data\%username%" >nul
-
 rem sometimes if the last user gets deleted the data folder will also be deleted
 rem this just makes the data folder if it dosnt exist
 if not exist "%cd%\data" mkdir "%cd%\data"
 goto admin
 
-:changeuserpassword
+rem admin lists all users
+:list
+cls
+echo All Users
+for /F "delims= eol=" %%A IN ('dir /AS /B') do echo %%~nA
+echo Press Any Key To Continue
+pause >nul
+goto admin
 
+:changeuserpassword
 goto admin
 
 rem change admin password
 :adminpassword
+rem asks user for current password  
 echo Please Enter The Admin Password:
 set /p pass= 
 if not %password% EQU %pass% (
@@ -205,6 +226,7 @@ echo Invalid Password.
 pause>nul
 goto main
 )
+rem asks user for new password  
 echo Enter New Admin Password
 set /p password=
 del /AS admin.bat
@@ -232,5 +254,15 @@ goto main
 
 :copied
 echo Link copied to clipboard
+pause>nul
+goto main
+
+:update
+echo Update 19/08/2019
+echo.
+echo.
+echo Changelog
+echo.
+echo Added this menu when launcher updates
 pause>nul
 goto main
