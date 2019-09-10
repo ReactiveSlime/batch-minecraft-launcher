080b8c4dc8646e27a6072df31f6da8a0e989d683
ReactiveSlime <33995597+ReactiveSlime@users.noreply.github.com>
2019-09-10 22:55:08 +1000

    now downlaods mc form minecraft.net

--- setup.bat
+++ setup.bat
@@ -2,10 +2,10 @@
 mkdir data
 mkdir bin
 echo Downloading Minecraft.exe
-curl -o bin\minecraft.exe "https://www.dropbox.com/s/u1iu5haq0jegspx/MinecraftLauncher.exe?dl=1" -L
+curl -o bin\minecraft.exe "https://launcher.mojang.com/download/Minecraft.exe" -L
 cls
 echo Downloading run.bat
-curl -o run.bat "https://www.dropbox.com/s/mavegevzttdbeqw/run.bat?dl=1" -L
+curl -o run.bat "https://raw.githubusercontent.com/ReactiveSlime/batch-minecraft-launcher/master/run.bat" -L
 (
 echo set username1=admin
 echo set password1=password
