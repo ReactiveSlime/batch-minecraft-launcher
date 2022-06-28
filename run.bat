745421b94faef104a0036013eb5da9d980269715
ReactiveSlime <33995597+ReactiveSlime@users.noreply.github.com>
2022-06-28 20:35:36 +1000

    Made selecting options more clear

--- run.bat
+++ run.bat
@@ -10,12 +10,12 @@ cls
 if not exist version.txt (
 echo|set /p="0">Version.txt
 )
-set current_build=5
+set current_build=6
 set /p build=<version.txt
 if %build% EQU %current_build% (
 goto argument
 ) else (
-echo|set /p="5">Version.txt
+echo|set /p="6">Version.txt
 goto update
 pause
 )
@@ -61,7 +61,7 @@ echo 2) Create Account
 echo 3) Request Feature Or Report A Bug
 echo.
 echo.
-set /p login=
+set /p login=Option: 
 if %login% EQU 1 goto login
 if %login% EQU 2 goto createuser
 if %login% EQU 3 goto links
@@ -109,7 +109,7 @@ echo 6) Open Minecraft Textures Folder
 echo 7) Log Out
 echo.
 echo.
-set /p settingchoise=
+set /p settingchoise=Option: 
 if %settingchoise% EQU 1 goto play
 if %settingchoise% EQU 2 goto accountdelete
 if %settingchoise% EQU 3 goto changepassword
@@ -136,7 +136,7 @@ goto loginsettings
 )
 rem ask user for new password
 echo What Would Uou Like To Change Your Password To?
-set /p password=
+set /p password=Password: 
 echo.
 del /AS "%name%.bat"
 (
@@ -163,7 +163,7 @@ cls
 echo.
 echo What Would You Like Your Username To Be?
 echo Username can't contain spaces
-set /p username= 
+set /p username=Username: 
 rem checks if account already exist
 if exist "%username%.bat" (
 echo The Account %username% Already Exist.
@@ -174,7 +174,7 @@ goto main
 echo.
 echo What Would You Like Your Password To Be?
 echo Password can't contain spaces
-set /p password= 
+set /p password=Password: 
 rem creats file with user info
 (
 echo set username=%username%
@@ -188,7 +188,7 @@ rem admin password check
 :admincheck
 call admin.bat
 echo Please Enter The Admin Password: 
-set /p pass=
+set /p pass=Password: 
 if not %password% EQU %pass% (
 echo Invalid Password.
 pause>nul
@@ -209,7 +209,7 @@ echo 4) Change User Password (WIP)
 echo 5) Change Admin Password
 echo.
 echo.
-set /p admin=
+set /p admin=Option: 
 if %admin% EQU 1 goto main
 if %admin% EQU 2 goto delete
 if %admin% EQU 3 goto list
@@ -251,7 +251,7 @@ rem change admin password
 :adminpassword
 rem asks user for current password  
 echo Please Enter The Admin Password:
-set /p pass= 
+set /p pass=Password: 
 if not %password% EQU %pass% (
 echo Invalid Password.
 pause>nul
@@ -259,7 +259,7 @@ goto main
 )
 rem asks user for new password  
 echo Enter New Admin Password
-set /p password=
+set /p password=Password: 
 del /AS admin.bat
 echo set password=%password%>admin.bat
 attrib +H +S admin.bat
@@ -276,7 +276,7 @@ echo 3) Request Feature
 echo 4) General Feedback
 echo.
 echo.
-set /p linkoption=
+set /p linkoption=Option: 
 if %linkoption% EQU 1 echo|set/p=http://PlanetMinecraft|clip & goto copied
 if %linkoption% EQU 2 echo|set/p=https://docs.google.com/forms/d/e/1FAIpQLSeHiob751XWvtVHlN_vs2p09wzJN5tnog4QRAqIn5X9n-3Azw/viewform|clip & goto copied
 if %linkoption% EQU 3 echo|set/p=https://docs.google.com/forms/d/e/1FAIpQLSeztMZDLQMTmcVu7L1sOQybVNHwGn9Zsl1PQJXVtb6E7hQtMQ/viewform|clip & goto copied
